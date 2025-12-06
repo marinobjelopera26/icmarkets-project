3ebabc4cfd706c1b07cd43e1ab796ee2be86a53e
Marino Bjelopera <MBjelopera@dspace.hr>
2025-12-06 20:53:13 +0100

    use and configure Serilog as logging provider

--- src/ICM.Crypto.WebApi/Program.cs
+++ src/ICM.Crypto.WebApi/Program.cs
@@ -1,10 +1,17 @@
 using ICM.Crypto.Application;
 using ICM.Crypto.Infrastructure.BlockCypher;
 using ICM.Crypto.Infrastructure.Persistence;
+using ICM.Crypto.WebApi.Logging;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+Log.Logger = SerilogLoggerConfiguration
+    .CreateLoggerConfiguration()
+    .CreateLogger();
+
+builder.Host.UseSerilog(Log.Logger, dispose: true);
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -17,6 +24,8 @@ builder.Services
     .AddPersistence();
 
 var app = builder.Build();
+
+app.UseSerilogRequestLogging();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -31,4 +40,16 @@ app.UseAuthorization();
 
 app.MapControllers();
 
-app.Run();
+try
+{
+    Log.Information("Starting web host ({Environment})", app.Environment.EnvironmentName);
+    await app.RunAsync();
+}
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Host terminated unexpectedly");
+}
+finally
+{
+    Log.CloseAndFlush();
+}
