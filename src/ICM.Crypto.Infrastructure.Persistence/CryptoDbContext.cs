48dc770bc00bd0ed3b0b7416643125226c1ae394
marinobjelopera26 <64930112+marinobjelopera26@users.noreply.github.com>
2025-12-06 22:10:08 +0100

    Setup persistence layer with EFCore and Postgres database (#8)
    * use and configure Postgres with EntityFrameworkCore
    * reference Microsoft.EntityFrameworkCore nuget package
    * extend Docker compose with Postgres container and Postgres admin UI container

---
+++ src/ICM.Crypto.Infrastructure.Persistence/CryptoDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ICM.Crypto.Infrastructure.Persistence;
+
+public sealed class CryptoDbContext : DbContext
+{
+    public CryptoDbContext(DbContextOptions<CryptoDbContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        // TODO: load entity configurations from assembly
+    }
+}
