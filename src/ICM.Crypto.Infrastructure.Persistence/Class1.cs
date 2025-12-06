8f21824ea5b627cf365e1c4fbaf74fc2de2859c9
Marino Bjelopera <MBjelopera@dspace.hr>
2025-12-06 18:21:00 +0100

    create necessary projects

---
+++ src/ICM.Crypto.Infrastructure.Persistence/Class1.cs
@@ -0,0 +1,5 @@
+﻿namespace ICM.Crypto.Infrastructure.Persistence;
+
+public class Class1
+{
+}
---
+++ tests/ICM.Crypto.IntegrationTests/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace ICM.Crypto.IntegrationTests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
