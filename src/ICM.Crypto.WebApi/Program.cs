fba76b2f3ccb447b64a590070816a6bcd43aee93
Marino Bjelopera <MBjelopera@dspace.hr>
2025-12-06 21:15:45 +0100

    configure API versioning with Swagger

--- src/ICM.Crypto.WebApi/Program.cs
+++ src/ICM.Crypto.WebApi/Program.cs
@@ -1,8 +1,13 @@
+using Asp.Versioning;
+using Asp.Versioning.ApiExplorer;
 using ICM.Crypto.Application;
 using ICM.Crypto.Infrastructure.BlockCypher;
 using ICM.Crypto.Infrastructure.Persistence;
 using ICM.Crypto.WebApi.Logging;
+using ICM.Crypto.WebApi.Swagger;
+using Microsoft.Extensions.Options;
 using Serilog;
+using Swashbuckle.AspNetCore.SwaggerGen;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -14,9 +19,30 @@ Log.Logger = SerilogLoggerConfiguration
 builder.Host.UseSerilog(Log.Logger, dispose: true);
 
 builder.Services.AddControllers();
+
+builder.Services
+    .AddApiVersioning(options =>
+    {
+        options.DefaultApiVersion = new ApiVersion(1, 0);
+        options.AssumeDefaultVersionWhenUnspecified = true;
+        options.ReportApiVersions = true;
+
+        options.ApiVersionReader = ApiVersionReader.Combine(
+            new UrlSegmentApiVersionReader(),
+            new QueryStringApiVersionReader("apiVersion"),
+            new HeaderApiVersionReader("X-Api-Version"));
+    })
+    .AddApiExplorer(options =>
+    {
+        options.GroupNameFormat = "'v'VVV";
+        options.SubstituteApiVersionInUrl = true;
+    });
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddTransient<IConfigureOptions<SwaggerGenOptions>, ConfigureSwaggerOptions>();
 
 builder.Services
     .AddApplication()
@@ -30,8 +56,18 @@ app.UseSerilogRequestLogging();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    var provider = app.Services.GetRequiredService<IApiVersionDescriptionProvider>();
+    
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(setup =>
+    {
+        foreach (var description in provider.ApiVersionDescriptions)
+        {
+            setup.SwaggerEndpoint(
+                url: $"/swagger/{description.GroupName}/swagger.json",
+                name: $"ICM Crypto API {description.GroupName.ToUpperInvariant()}");
+        }
+    });
 }
 
 app.UseHttpsRedirection();
