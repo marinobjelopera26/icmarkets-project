4ba4156a8241604e1f1ca5bd7e976077571ea1fb
Marino Bjelopera <MBjelopera@dspace.hr>
2025-12-06 18:21:00 +0100

    create necessary projects

---
+++ tests/ICM.Crypto.UnitTests/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace ICM.Crypto.UnitTests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
