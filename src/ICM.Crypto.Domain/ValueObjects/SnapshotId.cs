cb6c16b42e06063a0407ce948f96ad6a8fe3bcb3
marinobjelopera26 <64930112+marinobjelopera26@users.noreply.github.com>
2025-12-07 12:50:31 +0100

    Define initial domain layer aggregate root and value objects (#9)
    Defined initial aggregate root BlockchainSnapshot and necessary value objects to achieve type-safety and to enforce domain rules.

---
+++ src/ICM.Crypto.Domain/ValueObjects/SnapshotId.cs
@@ -0,0 +1,15 @@
+﻿namespace ICM.Crypto.Domain.ValueObjects;
+
+public sealed record SnapshotId
+{
+    public static SnapshotId CreateNew() => new(Guid.NewGuid());
+
+    private SnapshotId(Guid value)
+    {
+        Value = value;
+    }
+    
+    public Guid Value { get; }
+    
+    public override string ToString() => Value.ToString("D");
+}
---
+++ src/ICM.Crypto.Domain/ValueObjects/SourceUrl.cs
@@ -0,0 +1,24 @@
+﻿namespace ICM.Crypto.Domain.ValueObjects;
+
+public sealed record SourceUrl
+{
+    public static SourceUrl Create(string value)
+    {
+        if (!Uri.TryCreate(value, UriKind.Absolute, out var parsedUri) ||
+            (parsedUri.Scheme != Uri.UriSchemeHttp && parsedUri.Scheme != Uri.UriSchemeHttps))
+        {
+            throw new ArgumentException("");
+        }
+
+        return new SourceUrl(parsedUri.ToString());
+    }
+
+    private SourceUrl(string value)
+    {
+        Value = value;
+    }
+    
+    public string Value { get; }
+
+    public override string ToString() => Value;
+}
