a087169f00a45964554b479cef3a3e974f4f7297
Marino Bjelopera <MBjelopera@dspace.hr>
2025-12-08 14:35:01 +0100

    add request duration delegating handler

--- src/ICM.Crypto.Infrastructure.BlockCypher/DependencyInjection.cs
+++ src/ICM.Crypto.Infrastructure.BlockCypher/DependencyInjection.cs
@@ -6,12 +6,15 @@ public static class DependencyInjection
 {
     public static IServiceCollection AddBlockCypher(this IServiceCollection services)
     {
+        services.AddTransient<RequestDurationDelegatingHandler>();
+
         services.AddHttpClient<IBlockCypherService, BlockCypherService>(
             client =>
             {
                 client.BaseAddress = new Uri("https://api.blockcypher.com/v1/");
                 client.Timeout = TimeSpan.FromSeconds(15);
-            });
+            })
+            .AddHttpMessageHandler<RequestDurationDelegatingHandler>();
 
         return services;
     }
---
+++ src/ICM.Crypto.Infrastructure.BlockCypher/RequestDurationDelegatingHandler.cs
@@ -0,0 +1,31 @@
+﻿using System.Diagnostics;
+using Microsoft.Extensions.Logging;
+
+namespace ICM.Crypto.Infrastructure.BlockCypher;
+
+public class RequestDurationDelegatingHandler : DelegatingHandler
+{
+    private readonly ILogger _logger;
+
+    public RequestDurationDelegatingHandler(ILogger<RequestDurationDelegatingHandler> logger)
+    {
+        _logger = logger;
+    }
+    
+    protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+    {
+        var stopwatch = Stopwatch.StartNew();
+        var httpResponseMessage = await base.SendAsync(request, cancellationToken);
+        stopwatch.Stop();
+
+        var requestDuration = stopwatch.ElapsedMilliseconds;
+        
+        httpResponseMessage.Headers.Add("X-Client-Duration-Ms", requestDuration.ToString());
+        
+        _logger.LogInformation("HTTP request to '{RequestUrl}' finished after {RequestDuration}ms.",
+            httpResponseMessage.RequestMessage!.RequestUri,
+            requestDuration);
+        
+        return httpResponseMessage;
+    }
+}
