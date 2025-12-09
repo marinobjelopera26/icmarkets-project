2f9963f9de684fa92a30cc86fd7019896a3925b1
Marino Bjelopera <MBjelopera@dspace.hr>
2025-12-09 13:50:08 +0100

    add mediator abstractions

---
+++ src/ICM.Crypto.Application/Abstractions/Messaging/IQuery.cs
@@ -0,0 +1,7 @@
+﻿namespace ICM.Crypto.Application.Abstractions.Messaging;
+
+/// <summary>
+/// Represents a query that generates a result of  <typeparamref name="TResult"/> type.
+/// </summary>
+/// <typeparam name="TResult">Query result type.</typeparam>
+public interface IQuery<TResult>;
