10ecd651aadaa7673efb408377979cd353879781
marinobjelopera26 <64930112+marinobjelopera26@users.noreply.github.com>
2025-12-09 13:53:55 +0100

    Add mediator abstractions (#16)

---
+++ src/ICM.Crypto.Application/Abstractions/Messaging/ICommandHandler.cs
@@ -0,0 +1,24 @@
+﻿namespace ICM.Crypto.Application.Abstractions.Messaging;
+
+/// <summary>
+/// Represents a command handler for a <typeparamref name="TCommand"/> command that
+/// does not generator a result.
+/// </summary>
+/// <typeparam name="TCommand">Command type.</typeparam>
+public interface ICommandHandler<in TCommand>
+    where TCommand : ICommand
+{
+    Task HandleAsync(TCommand command, CancellationToken cancellationToken = default);
+}
+
+/// <summary>
+/// Represents a command handler for a <typeparamref name="TCommand"/> command that
+/// generates a result of <typeparamref name="TResult"/> type.
+/// </summary>
+/// <typeparam name="TCommand">Command type.</typeparam>
+/// <typeparam name="TResult">Command result type.</typeparam>
+public interface ICommandHandler<in TCommand, TResult>
+    where TCommand : ICommand<TResult>
+{
+    Task<TResult> HandleAsync(TCommand command, CancellationToken cancellationToken = default);
+}
---
+++ src/ICM.Crypto.Application/Abstractions/Messaging/IQueryHandler.cs
@@ -0,0 +1,13 @@
+﻿namespace ICM.Crypto.Application.Abstractions.Messaging;
+
+/// <summary>
+/// Represents a query handler that handles the <typeparamref name="TQuery"/> query
+/// which generates a result of <typeparamref name="TResult"/> type.
+/// </summary>
+/// <typeparam name="TQuery">Query type.</typeparam>
+/// <typeparam name="TResult">Query result type.</typeparam>
+public interface IQueryHandler<in TQuery, TResult>
+    where TQuery : IQuery<TResult>
+{
+    Task<TResult> HandleAsync(TQuery query, CancellationToken cancellationToken = default);
+}
