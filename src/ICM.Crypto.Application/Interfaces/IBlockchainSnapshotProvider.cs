14807b904024e9ccfe2f17078b928f7c9034248b
marinobjelopera26 <64930112+marinobjelopera26@users.noreply.github.com>
2025-12-10 09:34:29 +0100

    Modify IBlockchainSnapshotProvider interface and implementation (#17)

--- src/ICM.Crypto.Application/Interfaces/IBlockchainSnapshotProvider.cs
+++ src/ICM.Crypto.Application/Interfaces/IBlockchainSnapshotProvider.cs
@@ -1,21 +1,14 @@
-﻿namespace ICM.Crypto.Application.Interfaces;
+﻿using ICM.Crypto.Domain.ValueObjects;
+
+namespace ICM.Crypto.Application.Interfaces;
 
 public interface IBlockchainSnapshotProvider
 {
     /// <summary>
     /// Retrieves the latest snapshot of a single blockchain.
     /// </summary>
-    /// <param name="descriptor">Blockchain type descriptor.</param>
-    /// <param name="cancellationToken"><see cref="CancellationToken"/></param>
+    /// <param name="blockchain">Blockchain type.</param>
+    /// <param name="cancellationToken"><see cref="CancellationToken"/>.</param>
     Task<BlockchainSnapshotDto> GetSnapshotAsync(
-        BlockchainDescriptor descriptor, CancellationToken cancellationToken = default);
-    
-    /// <summary>
-    /// Retrieves latest snapshots of all specified blockchains described by the
-    /// <paramref name="descriptors"/> param.
-    /// </summary>
-    /// <param name="descriptors">Blockchain type descriptors.</param>
-    /// <param name="cancellationToken"><see cref="CancellationToken"/></param>
-    Task<IReadOnlyList<BlockchainSnapshotDto>> GetSnapshotsAsync(
-        IEnumerable<BlockchainDescriptor> descriptors, CancellationToken cancellationToken = default);
+        Blockchain blockchain, CancellationToken cancellationToken = default);
 }
