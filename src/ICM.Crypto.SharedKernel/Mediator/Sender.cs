753ae7292e5ea483611bb3c3e55e9bb706c8674c
marinobjelopera26 <64930112+marinobjelopera26@users.noreply.github.com>
2025-12-10 09:48:18 +0100

    Introduce lightweight mediator implementation (#18)

---
+++ src/ICM.Crypto.SharedKernel/Mediator/Sender.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace ICM.Crypto.SharedKernel.Mediator;
+
+internal sealed class Sender(IServiceProvider serviceProvider) : ISender
+{
+    public Task<TResponse> Send<TResponse>(IQuery<TResponse> query, CancellationToken cancellationToken)
+    {
+        var handlerType = typeof(IQueryHandler<,>).MakeGenericType(query.GetType(), typeof(TResponse));
+        dynamic handler = serviceProvider.GetRequiredService(handlerType);
+        return handler.Handle((dynamic)query, cancellationToken);
+    }
+
+    public Task<TResponse> Send<TResponse>(ICommand command, CancellationToken cancellationToken)
+    {
+        var handlerType = typeof(ICommandHandler<>).MakeGenericType(command.GetType());
+        dynamic handler = serviceProvider.GetRequiredService(handlerType);
+        return handler.Handle((dynamic)command, cancellationToken);
+    }
+
+    public Task<TResponse> Send<TResponse>(ICommand<TResponse> command, CancellationToken cancellationToken)
+    {
+        var handlerType = typeof(ICommandHandler<,>).MakeGenericType(command.GetType(), typeof(TResponse));
+        dynamic handler = serviceProvider.GetRequiredService(handlerType);
+        return handler.Handle((dynamic)command, cancellationToken);
+    }
+}
