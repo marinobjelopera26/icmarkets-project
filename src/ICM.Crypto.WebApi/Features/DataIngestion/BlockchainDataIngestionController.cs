ba9da07160b7cf59d3aeeac24ef6c89f06666531
marinobjelopera26 <64930112+marinobjelopera26@users.noreply.github.com>
2025-12-11 14:23:01 +0100

    Add endpoint for manual blockchain data ingestion (#21)

---
+++ src/ICM.Crypto.WebApi/Features/DataIngestion/BlockchainDataIngestionController.cs
@@ -0,0 +1,28 @@
+﻿using Asp.Versioning;
+using ICM.Crypto.Application.Abstractions.Messaging;
+using ICM.Crypto.Application.Features.DataIngestion;
+using ICM.Crypto.WebApi.Versioning;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ICM.Crypto.WebApi.Features.DataIngestion;
+
+[ApiController]
+[ApiVersion(ApiVersions.V1_0)]
+[Route("api/v{version:apiVersion}/blockchains/ingest")]
+public sealed class BlockchainDataIngestionController : ControllerBase
+{
+    [HttpPost]
+    [MapToApiVersion(ApiVersions.V1_0)]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    public async Task<IActionResult> PostAsync(
+        [FromBody] IngestBlockchainDataRequest request,
+        [FromServices] ICommandHandler<IngestSnapshotsCommand> handler,
+        CancellationToken cancellationToken)
+    {
+        var command = new IngestSnapshotsCommand(request.Blockchains);
+        
+        await handler.HandleAsync(command, cancellationToken);
+
+        return Ok("Successfully ingested blockchain snapshot data.");
+    }
+}
