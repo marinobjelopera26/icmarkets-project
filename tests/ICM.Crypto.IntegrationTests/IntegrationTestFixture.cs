4f061ff495447e1f6a72d1cc75d5b51db8741772
Marino Bjelopera <MBjelopera@dspace.hr>
2025-12-13 18:03:05 +0100

    try change integration test image path

--- tests/ICM.Crypto.IntegrationTests/IntegrationTestFixture.cs
+++ tests/ICM.Crypto.IntegrationTests/IntegrationTestFixture.cs
@@ -44,7 +44,7 @@ public class IntegrationTestFixture
         await _pgContainer.StartAsync();
         
         _apiContainer = new ContainerBuilder()
-            .WithImage("icm/blockchain.api:integration")
+            .WithImage("gchr.io/marinobjelopera26/icm/blockchain.api:integration")
             .WithNetwork(_network)
             .WithNetworkAliases("webapi")
             .WithEnvironment("ASPNETCORE_URLS", $"http://0.0.0.0:{ApiPort}")
